a25f931044b34e3ea338fca8d939aefce91e9b62
Syed Adeel Ashraf <syedadeelashraf123@gmail.com>
2023-11-02 03:41:59 +0530

    feat: learned how to print the varibale which contains integer by converting them into str

---
+++ Code2.py
@@ -0,0 +1,14 @@
+# Mathematical Basic problems in python 
+a = 10
+b = 22
+c = a+b
+print("Answer of Addition: " + str(c))
+d = 24
+e = c * d
+print("Answer for Multiplication: " + str(e))
+f = 39
+g = e - f 
+print("Answer for Subtraction: " + str(g))
+h = 900
+i = h/g
+print("Answer for Division: " + str(i))
