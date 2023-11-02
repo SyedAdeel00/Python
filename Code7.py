914de3a82184f12032b650ee849f837582779792
Syed Adeel Ashraf <syedadeelashraf123@gmail.com>
2023-11-03 00:45:44 +0530

    while loop and range method

---
+++ Code7.py
@@ -0,0 +1,15 @@
+# range in python
+number = range(5)
+print(number)
+
+# while loops 
+i = 1
+while i <= 5 :
+    print(i)
+    i += 1 # i = i +1
+
+    #pattern print
+    a = 1
+    while a <= 5:
+        print( a * "*")
+        a += 1
