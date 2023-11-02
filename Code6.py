d991d4cde7cd00b798fd502ae5904e469eb071df
Syed Adeel Ashraf <syedadeelashraf123@gmail.com>
2023-11-02 23:47:47 +0530

    else if in python

---
+++ Code6.py
@@ -0,0 +1,9 @@
+# else if 
+a = 10
+
+if a >= 18 :
+    print("you are eligible for vote")
+elif a < 18 and a > 3 :
+    print(" you are note eligible complete your schooling")
+else: 
+    print("you are a child !")
